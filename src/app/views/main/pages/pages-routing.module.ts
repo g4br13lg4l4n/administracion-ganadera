76fbca8f39ecfa7b8756bacc750e7ac1923da56e
Gabriel Galan <gabriel.galan@mother.travel>
2023-10-24 16:12:44 -0600

    fix component

--- src/app/views/main/pages/pages-routing.module.ts
+++ src/app/views/main/pages/pages-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmployeesComponent } from './employees/employees.component';
+import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: EmployeesComponent
+    component: ListEmployeesComponent
   },
 ];
 
