6dc545986d517374588932b42f1f928bfd6c0a57
Gabriel Galán <gabriel.galan92@gmail.com>
2023-10-30 17:07:41 -0600

    advance form

--- src/app/views/main/components/form-modal-cattle/form-modal-cattle.component.ts
+++ src/app/views/main/components/form-modal-cattle/form-modal-cattle.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DialogOptions } from 'src/app/core/models/dialogOptions';
 
 @Component({
   selector: 'app-form-modal-cattle',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-modal-cattle.component.css']
 })
 export class FormModalCattleComponent {
+  constructor(
+    public dialogRef: MatDialogRef<FormModalCattleComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogOptions
+  ){
+    console.log('-->', this.data)
+  }
 
+  close(): void {
+    this.dialogRef.close('enviar algo');
+  }
 }
--- src/app/views/main/components/form-modal-supplies/form-modal-supplies.component.ts
+++ src/app/views/main/components/form-modal-supplies/form-modal-supplies.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DialogOptions } from 'src/app/core/models/dialogOptions';
 
 @Component({
   selector: 'app-form-modal-supplies',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-modal-supplies.component.css']
 })
 export class FormModalSuppliesComponent {
+  constructor(
+    public dialogRef: MatDialogRef<FormModalSuppliesComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogOptions
+  ){
+    console.log('-->', this.data)
+  }
 
+  close(): void {
+    this.dialogRef.close('enviar algo');
+  }
 }
